48844020a5db91fb3c811139c127e529c7120731
fjque@LAPTOP-H7OVTKMQ MINGW32 <fquenard@gmx.fr>
2019-10-12 18:58:22 +0200

    ajout d'une balise div pour l'erreur d'un formulaire vide

--- Views/Adminloginview.php
+++ Views/Adminloginview.php
@@ -6,6 +6,8 @@
                 <h5 class="card-header">Formulaire de connexion</h5>
                 <div class="card-body">
                         <form action="index.php?controller=backEnd&action=checkLogin" method="post">
+                        <div id="error_password"></div>
+
                                 <label for="pseudoAdmin">Identifiant :</label> <input type="text" name="pseudoAdmin" class="form-control" id="pseudoAdmin" />
                                 <label for="loginAdmin">Mot de passe :</label> <input type="password" name="loginAdmin" class="form-control" id="loginAdmin" /><br />
 
